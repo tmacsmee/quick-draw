88911ae106851abc095ae865f6d2072e44bafa20
un-fathom <troymacsmee@gmail.com>
2022-09-15 18:16:48 +1200

    Added password authentication method in JSON parser

--- src/main/java/nz/ac/auckland/se206/util/JsonParser.java
+++ src/main/java/nz/ac/auckland/se206/util/JsonParser.java
@@ -1,21 +1,35 @@
 package nz.ac.auckland.se206.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Map;
 
 public class JsonParser {
 
-    private final ObjectMapper mapper = new ObjectMapper();
+  private final ObjectMapper mapper = new ObjectMapper();
+  private Map<String, Map<String, Object>> allUserData;
 
-    public Map<String, Map<String, Object>> parseAllUserData() throws IOException {
-        return mapper.readValue(Paths.get("user_files/user_data.json").toFile(), Map.class);
+  public JsonParser() {
+    try {
+      allUserData = mapper.readValue(Paths.get("user_files/user_data.json").toFile(), Map.class);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    public Object getProperty(String user, String property) throws IOException {
-        Map<String, Map<String, Object>> allUserData = parseAllUserData();
-        return allUserData.get(user).get(property);
-    }
+  public Object getProperty(String username, String property) {
+    return allUserData.get(username).get(property);
+  }
+
+  /**
+   * Checks if the given password is correct for the given username
+   *
+   * @param username The username of the user
+   * @param password The password to check
+   * @return True if the password is correct, false otherwise
+   */
+  public boolean isCorrectPassword(String username, String password) {
+    return getProperty(username, "password").equals(password);
+  }
 }
--- src/test/java/nz/ac/auckland/se206/util/JsonParserTest.java
+++ src/test/java/nz/ac/auckland/se206/util/JsonParserTest.java
@@ -1,18 +1,27 @@
 package nz.ac.auckland.se206.util;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class JsonParserTest {
 
-    @Test
-    void testGetUsers() throws IOException {
-        JsonParser parser = new JsonParser();
-        assertEquals("123", parser.getProperty("username1", "password"));
-        assertEquals("test1", ((List<String>) parser.getProperty("username2", "wordsEncountered")).get(0));
-    }
+  private JsonParser parser;
+
+  @Test
+  void testGetUsers() throws IOException {
+    parser = new JsonParser();
+    assertEquals("123", parser.getProperty("username1", "password"));
+    assertEquals(
+        "test1", ((List<String>) parser.getProperty("username2", "wordsEncountered")).get(0));
+  }
+
+  @Test
+  void testIsCorrectPassword() throws IOException {
+    parser = new JsonParser();
+    assertTrue(parser.isCorrectPassword("username1", "123"));
+    assertFalse(parser.isCorrectPassword("username1", "1234"));
+  }
 }

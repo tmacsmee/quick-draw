b8e13e84f6f5d9d44a77c128d563c613afa70943
Michael Hardy <mhar860@aucklanduni.ac.nz>
2022-09-20 21:10:00 +1200

    game no longer makes predictions when the canvas is empty, removed _ in predictionsList, updated login.fxml UI

--- src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CanvasController.java
@@ -36,6 +36,9 @@ public class CanvasController {
   @FXML private Button brushButton;
   @FXML private Button eraserButton;
 
+  // boolean used to check if the canvas is empty or not
+  private boolean isCanvasNotEmpty = false;
+
   /**
    * Initializes the drawing canvas, mouse input, and machine learning model
    *
@@ -72,6 +75,9 @@ public class CanvasController {
           double thisX = e.getX(); // Get coordinates of mouse on drag.
           double thisY = e.getY();
 
+          // canvas declared as not empty when user starts drawing
+          isCanvasNotEmpty = true;
+
           graphic.setLineWidth(brushSize);
           graphic.setStroke(Color.BLACK);
           graphic.strokeLine(
@@ -102,6 +108,11 @@ public class CanvasController {
   @FXML
   void onClear() {
     graphic.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+    // when canvas is cleared, canvas declared as empty and predictionList no longer displays
+    // anything
+    isCanvasNotEmpty = false;
+    predictionList.setText("");
   }
 
   /** Creates and starts the task that manages the time limit. */
@@ -125,7 +136,11 @@ public class CanvasController {
    * @param predictions list of predictions
    */
   public void setPredictionList(String predictions) {
-    predictionList.setText(predictions);
+
+    // only display predictions if canvas is not empty
+    if (isCanvasNotEmpty) {
+      predictionList.setText(predictions);
+    }
   }
 
   /**
@@ -152,10 +167,17 @@ public class CanvasController {
    * @throws TranslateException If there is an error in translating the prompt to a classification.
    */
   public boolean isCorrect() throws TranslateException {
-    for (Classifications.Classification c :
-        model.getPredictions(getCurrentSnapshot(), 3)) { // Get the top 3 predictions.
-      if (promptLabel.getText().substring(6).equalsIgnoreCase(c.getClassName().replace("_", " "))) {
-        return true; // If the prompt is in the top 3 predictions, return true.
+
+    // only predict if canvas is not empty
+    if (isCanvasNotEmpty) {
+      for (Classifications.Classification c :
+          model.getPredictions(getCurrentSnapshot(), 3)) { // Get the top 3 predictions.
+        if (promptLabel
+            .getText()
+            .substring(6)
+            .equalsIgnoreCase(c.getClassName().replace("_", " "))) {
+          return true; // If the prompt is in the top 3 predictions, return true.
+        }
       }
     }
     return false;
@@ -163,6 +185,7 @@ public class CanvasController {
 
   /** Switches to the 'results' scene. */
   public void results() {
+    predictionList.setText("");
     Scene thisScene = clearButton.getScene();
     thisScene.setRoot(SceneManager.getUiRoot((SceneManager.AppUi.RESULTS)));
   }

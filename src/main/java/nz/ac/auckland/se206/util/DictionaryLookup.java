4413fbe645f63c9538d0acfdb0f13ce16716ba1d
un-fathom <troymacsmee@gmail.com>
2022-10-13 19:12:05 +1300

    feat: added definitions file and updated dictionary lookup

--- src/main/java/nz/ac/auckland/se206/util/DictionaryLookup.java
+++ src/main/java/nz/ac/auckland/se206/util/DictionaryLookup.java
@@ -1,16 +1,11 @@
 package nz.ac.auckland.se206.util;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 import java.io.IOException;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
+import java.util.HashMap;
 
 public class DictionaryLookup {
-
-  private static final String API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
   /**
    * Returns the definition of the given word.
@@ -20,30 +15,13 @@ public class DictionaryLookup {
    * @throws IOException If the API call fails.
    */
   public String getDefinition(String word) throws IOException {
-    // Setup the HTTP client for request
-    OkHttpClient client = new OkHttpClient();
-    Request request = new Request.Builder().url(API_URL + word).build();
-    Response response = client.newCall(request).execute();
-    ResponseBody responseBody = response.body();
+
+    ObjectMapper mapper = new ObjectMapper();
 
     // Retrieve the information as JSON string
-    String jsonString = responseBody.string();
+    HashMap<String, HashMap<String, String>> definitions =
+        mapper.readValue(new File("src/main/resources/definitions.json"), HashMap.class);
 
-    // Parse the JSON string, if there is no definition, return word not found
-    try {
-      JsonNode jsonNode = new ObjectMapper().readTree(jsonString);
-      String definition =
-          jsonNode
-              .get(0)
-              .get("meanings")
-              .get(0)
-              .get("definitions")
-              .get(0)
-              .get("definition")
-              .asText();
-      return definition;
-    } catch (Exception e) {
-      return "Word not found";
-    }
+    return definitions.get("E").get(word);
   }
 }

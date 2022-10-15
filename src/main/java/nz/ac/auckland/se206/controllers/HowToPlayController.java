74c8648eadc247dd7b0a2d6f960b2e79b36a63aa
un-fathom <troymacsmee@gmail.com>
2022-10-15 16:16:30 +1300

    fix: changed play button destination on how to play scene to game mode scene

--- src/main/java/nz/ac/auckland/se206/controllers/HowToPlayController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/HowToPlayController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import nz.ac.auckland.se206.SceneManager;
-import nz.ac.auckland.se206.util.TextToSpeechTask;
 
 public class HowToPlayController {
 
@@ -34,12 +33,8 @@ public class HowToPlayController {
    */
   @FXML
   private void onPlay(ActionEvent event) {
-
     Button button = (Button) event.getSource(); // Get the scene of the button and switch its root.
     Scene buttonScene = button.getScene();
-    buttonScene.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.READY));
-
-    TextToSpeechTask textToSpeechTask = new TextToSpeechTask();
-    new Thread(textToSpeechTask).start();
+    buttonScene.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.GAMEMODE));
   }
 }
